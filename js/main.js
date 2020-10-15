3f62d530cda4bb3bf60bffd8a806ad6c9ff8e922
Antonia P <direiellie@gmail.com>
2020-10-15 16:36:25 +0200

    Mail - Completed

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 //ESERCITAZIONE MAIL - GIOCO DADI
 
 
-//LISTA/ARRAY EMAIL UTENTI
-var accounts = ['davide@boolean.it' , 'claudio@boolean.it' , 'francesca@boolean.it'];
-console.log = ('Accounts :' + accounts);
+
 
 //CHIEDO ALL'UTENTE LA SUA EMAIL
 var email = prompt('Inserisci la tua email');
-console.log = ('Email utente :' + email);
+console.log('');
+
+//LISTA/ARRAY EMAIL UTENTI
+var accounts = ['davide@boolean.it' , 'claudio@boolean.it' , 'francesca@boolean.it'];
+console.log('Accounts :' + accounts);
 
 
 //CONTROLLO SE E' NELLA LISTA DI CHI PUO' ACCEDERE E IMPLEMENTO UNA VARIABILE DI STATO FALSE PER LA VERIFICA
 
 var emailFound = false;
 
-for ( var i = 0; i < accounts.length; i++); {
+for ( var i = 0; (i < accounts.length) && (emailFound == false); i++); {
   var item = accounts [i];
-  if (email == item) {
-    emailFound = true;
-  }
+    console.log ('iterazione' + i + ':' + item);
 }
 
-if (emailFound = true); {
-  console.log = ('Email valida. Accedi.');
+if (email = item) {
+  emailFound = true;
+  console.log('Email valida.');
 } else {
-  console.log = ('Errore. Email non valida. Non puoi accedere.');
+  console.log('Errore. Email non valida.');
+}
+
+if (emailFound == true) {
+  console.log('Accesso abilitato.');
+} else {
+  console.log('Accesso negato.');
 }
